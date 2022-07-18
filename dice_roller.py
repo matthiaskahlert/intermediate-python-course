f24a478f1b8a998564c6fd13ef80aac0a6de023a
Matthias Kahlert <m-kahlert@web.de>
2022-07-18 16:48:20 +0200

    imported random module and added it to the roll variable

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,7 @@
+import random
 def main():
-  print('You rolled a die')
-
+	print('You rolled a die')
+	roll = random.randint(1,20)
+	print(f'You rolled a {roll}')
 if __name__== "__main__":
   main()
